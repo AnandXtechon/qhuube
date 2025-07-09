481ecc07e6c387b62fda3d2d52afa5bddb627dc5
AnandXtechon <anand.pande@xtechon.com>
2025-07-09 21:46:34 +0530

    Push

--- src/app/pricing/stripe/session/[sessionId]/route.ts
+++ src/app/pricing/stripe/session/[sessionId]/route.ts
@@ -1,22 +1,20 @@
-import { NextResponse } from "next/server";
-import Stripe from "stripe";
-
+import { NextRequest, NextResponse } from 'next/server'
+import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
-export async function GET(_req: Request, { params }: { params: { sessionId: string } }) {
+export async function GET(req: NextRequest, context: { params: { sessionId: string } }) {
+    const { sessionId } = context.params
+
     try {
-        const session = await stripe.checkout.sessions.retrieve(params.sessionId, {
-            expand: ['line_items', 'payment_intent'], 
-        });
-          
+        const session = await stripe.checkout.sessions.retrieve(sessionId, {
+            expand: ['line_items', 'payment_intent'],
+        })
+
         return NextResponse.json(session)
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
-        console.log('Stripe fetch error: ', err.message)
-        return NextResponse.json(
-            { error: 'Failed to fetch stripe session' },
-            { status: 500 }
-        )
+        console.error('Stripe fetch error:', err.message)
+        return NextResponse.json({ error: 'Failed to fetch Stripe session' }, { status: 500 })
     }
 }
