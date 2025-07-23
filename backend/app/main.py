3b6a393e5c759329bf82dd92f44cce4ea7fc46ce
AnandXtechon <anand.pande@xtechon.com>
2025-07-23 20:32:00 +0530

    Backend Updates

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app.include_router(product.router, prefix="/api/v1", tags=["Product"])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://qhuube.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
